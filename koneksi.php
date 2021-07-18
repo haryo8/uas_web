ffa931e53ac51b9043b14b5c445fe68dd60efa11
haryo8 <77155869+haryo8@users.noreply.github.com>
2021-07-18 13:02:48 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php 
+// isi nama host, username mysql, dan password mysql anda
+$conn = mysqli_connt("localhost","root","","1","haryo");
+
+if(!$conn){
+	echo "gagal konek database menn";
+} else {
+	
+};
+
+?>
